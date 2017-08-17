a33c4abc67ee8f3df040aa3dce7da7e48288ecc7
danielgrisof <danielgrisof@bitbucket.org>
2017-08-17 19:05:01 -0300

    Initial Commit

---
+++ PrecoBitcoin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PrecoBitcoin
+//
+//  Created by Daniel Griso Filho on 8/17/17.
+//  Copyright © 2017 Daniel Griso Filho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
